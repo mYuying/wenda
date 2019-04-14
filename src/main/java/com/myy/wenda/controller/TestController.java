a03713002989f4c075e00e96751c31210f2e5184
mYuying <18710893075@163.com>
2019-04-14 21:57:16 +0800

    add second function

--- src/main/java/com/myy/wenda/controller/TestController.java
+++ src/main/java/com/myy/wenda/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.myy.wenda.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class TestController {
     @RequestMapping("/fun1")
     public String test() {
         return "Hello , dear myy !";
+    }
+
+    @RequestMapping("/fun2")
+    public String fun2(@RequestParam("name") String name) {
+        return "I love " + name;
     }
 }
